bd62a7e861722f93e0aff69daa9e28c629b23a60
juarezadolfo <juarezadolfo@hotmail.com>
2018-02-13 14:46:07 -0800

    added TenantMain files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TenantSignup from "./pages/TenantSignup";
-
+import TenantMain from "./pages/TenantMain";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -9,7 +10,8 @@ class App extends Component {
       <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={TenantSignup} />
+          <Route exact path="/" component={TenantMain} />
+          
         </Switch>
       </div>
     </Router>
