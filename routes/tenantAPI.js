3c0a5ca62e299b8c4a3ccf996c67b0d18684a2bd
buzzhaha <boming.fan@gmail.com>
2018-02-16 11:32:39 -0800

    deploy

--- routes/tenantAPI.js
+++ routes/tenantAPI.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
 
     // See the react auth blog in which cors is required for access
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3001');
   res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
   next();
 });
