67ba2051555f14e3ca8a7f3a363f73457828c72f
buzzhaha <boming.fan@gmail.com>
2018-02-16 12:08:12 -0800

    domain

--- client/src/components/withAuth.js
+++ client/src/components/withAuth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import AuthService from './AuthService';
 
 export default function withAuth(AuthComponent) {
-    const Auth = new AuthService('https://localhost:3000');
+    const Auth = new AuthService('https://communitysteward.herokuapp.com');
     return class AuthWrapped extends Component {
 
         constructor() {
