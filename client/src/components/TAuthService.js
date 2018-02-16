506b8915f5313dc7a15300ddb12224894a304d41
buzzhaha <boming.fan@gmail.com>
2018-02-16 12:15:54 -0800

    domain change

--- client/src/components/TAuthService.js
+++ client/src/components/TAuthService.js
@@ -3,7 +3,7 @@ import decode from 'jwt-decode';
 export default class TAuthService {
     // Initializing important variables
     constructor(domain) {
-        this.domain = domain || 'https://localhost:3000' // API server domain
+        this.domain = domain || 'https://communitysteward.herokuapp.com' // API server domain
         this.fetch = this.fetch.bind(this) // React binding stuff
         this.login = this.login.bind(this)
         this.getProfile = this.getProfile.bind(this)
